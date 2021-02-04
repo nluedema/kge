7ecd3917c312dbf739f2806742dc5a13ec147479
nluedema <nluedema@mail.uni-mannheim.de>
2021-02-04 15:38:17 +0100

    Add DKRL meta model and embedder
    - model
      - uses a bases scorer, which can be any scorer
      - the base scorer is used to score combinations of structural and
    multimodal embeddings
      - only works with dkrl_embedder, because of the embedding return
    object
    - embedder
      - creates structural and multimodal embeddings, which are passed on to
    the scorer
    - TODO: allow more than "struct" plus one modality

---
+++ kge/model/dkrl_model.py
@@ -0,0 +1,74 @@
+import torch
+from kge import Config, Dataset
+from kge.model.kge_model import RelationalScorer, KgeModel
+
+import importlib
+
+class DKRLScorer(RelationalScorer):
+
+    def __init__(
+        self,
+        config: Config,
+        dataset: Dataset,
+        configuration_key=None
+    ):
+        super().__init__(config, dataset, configuration_key)
+        try:
+            base_scorer_class_name = self.get_option("base_scorer.class_name")
+            base_scorer_type = self.get_option("base_scorer.type")
+            module = importlib.import_module(f"kge.model.{base_scorer_type}")
+        except:
+            raise Exception(f"Can't find {configuration_key}.base_scorer in config")
+
+        try:
+            self.base_scorer = getattr(module, base_scorer_class_name)(
+                config=config,
+                dataset=dataset,
+                configuration_key=configuration_key + ".base_scorer"
+            )
+        except:
+            raise Exception(f"Can't find {base_scorer_class_name}")
+
+        self.modality_weight = self.get_option("modality_weight")
+
+    def score_emb(self, s_emb, p_emb, o_emb, combine: str):
+        s_emb_struct = s_emb[:,:,0]
+        o_emb_struct = o_emb[:,:,0]
+
+        s_emb_multimodal = s_emb[:,:,1]
+        o_emb_multimodal = o_emb[:,:,1]
+
+        score_ss = self.base_scorer.score_emb(
+            s_emb_struct, p_emb, o_emb_struct, combine
+        )
+        score_mm = self.base_scorer.score_emb(
+            s_emb_multimodal, p_emb, o_emb_multimodal, combine
+        )
+        score_ms = self.base_scorer.score_emb(
+            s_emb_multimodal, p_emb, o_emb_struct, combine
+        )
+        score_sm = self.base_scorer.score_emb(
+            s_emb_struct, p_emb, o_emb_multimodal, combine
+        )
+        score = score_ss + self.modality_weight * (score_mm + score_ms + score_sm)
+        return score
+
+class DKRLModel(KgeModel):
+
+    def __init__(
+        self,
+        config: Config,
+        dataset: Dataset,
+        configuration_key=None,
+        init_for_load_only=False,
+    ):
+        super().__init__(
+            config=config,
+            dataset=dataset,
+            scorer=DKRLScorer,
+            configuration_key=configuration_key,
+            init_for_load_only=init_for_load_only,
+        )
+
+        if self.get_option("entity_embedder.type") != "dkrl_embedder":
+            raise ValueError("dkrl_model only works with dkrl_embedder")
---
+++ kge/model/embedder/dkrl_embedder.py
@@ -0,0 +1,112 @@
+import torch
+from torch import Tensor
+
+from kge import Config, Dataset
+from kge.job import Job
+from kge.model import KgeEmbedder
+
+from typing import List
+
+class DKRLEmbedder(KgeEmbedder):
+
+    def __init__(
+        self,
+        config: Config,
+        dataset: Dataset,
+        configuration_key: str,
+        vocab_size: int,
+        init_for_load_only=False,
+    ):
+        super().__init__(
+            config, dataset, configuration_key, init_for_load_only=init_for_load_only
+        )
+
+        # read config
+        self.config.check("train.trace_level", ["batch", "epoch"])
+        self.vocab_size = vocab_size
+
+        if len(self.get_option("modalities")) > 2:
+            raise ValueError("dkrl_embedder only works with 2 modalities")
+
+        # create embedder for each modality
+        self.embedder = torch.nn.ModuleDict()
+        for modality in self.get_option("modalities"):
+            # if dim of modality embedder is < 0 set it to parent embedder dim
+            # e.g. when using dkrl, the text embedding dim should equal embedding dim
+            # but when using literale, the text embedding dim can vary
+            if self.get_option(f"{modality}.dim") < 0:
+                config.set(
+                    f"{self.configuration_key}.{modality}.dim",
+                    self.dim
+                )
+            
+            embedder = KgeEmbedder.create(
+                config, dataset, f"{self.configuration_key}.{modality}",
+                vocab_size=self.vocab_size, init_for_load_only=init_for_load_only
+            )
+            self.embedder[modality] = embedder
+        
+        # HACK
+        # kwargs["indexes"] is set to None, if dkrl_embedder has
+        # regularize_args.weighted set to False.
+        # If the child_embedder has regularize_args.weighted set to True,
+        # it tries to access kwargs["indexes"], which leads to an error
+
+        # Set regularize_args.weighted to True, if it is set for the struct embedder
+        if self.embedder["struct"].get_option("regularize_args.weighted"):
+            config.set(
+                self.configuration_key + ".regularize_args.weighted",
+                True
+            )
+        
+        # TODO handling negative dropout because using it with ax searches for now
+        dropout = self.get_option("dropout")
+        if dropout < 0:
+            if config.get("train.auto_correct"):
+                config.log(
+                    "Setting {}.dropout to 0, "
+                    "was set to {}.".format(configuration_key, dropout)
+                )
+                dropout = 0
+        self.dropout = torch.nn.Dropout(dropout)
+
+    def prepare_job(self, job: Job, **kwargs):
+        super().prepare_job(job, **kwargs)
+        for modality in self.get_option("modalities"):
+            self.embedder[modality].prepare_job(job, **kwargs)
+    
+    def _embed(self, indexes: Tensor) -> Tensor:
+        embeddings_list = []
+        for modality in self.get_option("modalities"):
+            embeddings_list.append(self.embedder[modality].embed(indexes))
+        # concatenate the embeddings along a new dimension
+        # creates tensor of shape N*dim*(nbr of modalities) 
+        embeddings = torch.stack(embeddings_list, dim=2)
+
+        return embeddings
+    
+    def embed(self, indexes: Tensor) -> Tensor:
+        return self._postprocess(self._embed(indexes.long()))
+
+    def embed_all(self) -> Tensor:
+        return self._postprocess(self._embeddings_all())
+
+    def _postprocess(self, embeddings: Tensor) -> Tensor:
+        if self.dropout.p > 0:
+            embeddings = self.dropout(embeddings)
+        return embeddings
+
+    def _embeddings_all(self) -> Tensor:
+        return self._embed(
+            torch.arange(
+                self.vocab_size, dtype=torch.long, device=self.config.get("job.device")
+            )
+        )
+
+    def penalty(self, **kwargs) -> List[Tensor]:
+        result = super().penalty(**kwargs)
+        # get penalty of modality embedders
+        for modality in self.get_option("modalities"):
+            result += self.embedder[modality].penalty(**kwargs)
+        
+        return result
