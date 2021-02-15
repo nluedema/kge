57f3bc5c1ef6b9d5b2c6a1cf867d68cfc8801db1
nluedema <nluedema@mail.uni-mannheim.de>
2021-02-15 18:39:15 +0100

    Adjust preprocessing and data for fb15k-237
    - switch from LiteralE to KBLRN numeric data
    - remove duplicates for fb15k-237 text preprocessing
      - has no efffect, as text data has no duplicates for fb15k-237
      - makes it more similar to yago3-10 text preprocessing

--- experiments/fb15k-237/original_files/explore_data.py
+++ experiments/fb15k-237/original_files/explore_data.py
@@ -1,12 +1,19 @@
 import pandas as pd
 data_path = "/work-ceph/nluedema/kge/experiments/fb15k-237/"
+
 numeric = pd.read_csv(
-    f"{data_path}original_files/LiteralE/numerical_literals.txt",
+    f"{data_path}original_files/KBLRN/FB15K_NumericalTriples.txt",
     names=["ent","rel","val"], header=None,sep="\t"
 )
-
 numeric[["ent","rel"]].duplicated().sum()
-# => 51471 duplicates 
+# => 0 duplicates
+
+#numeric = pd.read_csv(
+#    f"{data_path}original_files/LiteralE/numerical_literals.txt",
+#    names=["ent","rel","val"], header=None,sep="\t"
+#)
+#numeric[["ent","rel"]].duplicated().sum()
+## => 51471 duplicates 
 
 text = pd.read_csv(
     f"{data_path}original_files/DKRL/FB15k_mid2description.txt",
--- experiments/fb15k-237/preprocessed_files/preprocess.py
+++ experiments/fb15k-237/preprocessed_files/preprocess.py
@@ -17,7 +17,9 @@ with open(text_path, "r") as f:
 # Replace "\n" with " " and remove \"
 # Remove @en and surrounding quotes
 # nltk.word_tokenizer transforms "..." into ``...''
+# Remove duplicates
 # Remove entities that are not in fb15k-237
+ent_set = set()
 text_path_preprocessed = f"{data_path}preprocessed_files/text_data.txt"
 with open(text_path_preprocessed, "w") as f:
     for t in text_data:
@@ -29,22 +31,45 @@ with open(text_path_preprocessed, "w") as f:
             raise ValueError("Something went wrong")
         if text[0] != '"' and text[-4] != '"':
             raise ValueError("Something went wrong")
+        if ent not in ent_set:
+            ent_set.add(ent)
+        else:
+            continue
         if ent not in entities:
             continue
         f.write(f"{ent}\thasDescription\t{text[1:-4]}\n")
 
-numeric_path = f"{data_path}original_files/LiteralE/numerical_literals.txt"
-numeric_data = pd.read_csv(numeric_path,header=None, names=["ent","rel","val"] ,sep="\t")
-
-# Remove duplicates
-numeric_data = numeric_data.drop_duplicates(
-    subset=["ent","rel"],keep="last"
-)
+numeric_path = f"{data_path}original_files/KBLRN/FB15K_NumericalTriples.txt"
+with open(numeric_path, "r") as f:
+    numeric_data = list(
+        map(lambda s: s.strip().split("\t"), f.readlines())
+    )
 
 # Remove entities that are not in fb15k-237
 numeric_path_preprocessed = f"{data_path}preprocessed_files/numeric_data.txt"
 with open(numeric_path_preprocessed, "w") as f:
-    for ent, rel, val in numeric_data.values:
+    for t in numeric_data:
+        ent = t[0]
+        rel = t[1]
+        val = t[2]
+
         if ent not in entities:
             continue
         f.write(f"{ent}\t{rel}\t{val}\n")
+
+#numeric_path = f"{data_path}original_files/LiteralE/numerical_literals.txt"
+#numeric_data = pd.read_csv(numeric_path,header=None, names=["ent","rel","val"] ,sep="\t")
+#
+## Remove duplicates
+#numeric_data = numeric_data.drop_duplicates(
+#    subset=["ent","rel"],keep="last"
+#)
+#
+## Remove entities that are not in fb15k-237
+#numeric_path_preprocessed = f"{data_path}preprocessed_files/numeric_data.txt"
+#with open(numeric_path_preprocessed, "w") as f:
+#    for ent, rel, val in numeric_data.values:
+#        if ent not in entities:
+#            continue
+#        f.write(f"{ent}\t{rel}\t{val}\n")
+#
