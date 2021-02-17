65cf515824b774b796de4038f896b83a23f341f5
nluedema <nluedema@mail.uni-mannheim.de>
2021-02-17 17:45:16 +0100

    Adjust numeric_feature_embedder
    - normalization is now per relation and before zeros are imputed
    - added possibility to use mlp, to get a numeric embedding of a
    specfified size

--- kge/model/embedder/numeric_feature_embedder.py
+++ kge/model/embedder/numeric_feature_embedder.py
@@ -7,7 +7,34 @@ from kge.model import KgeEmbedder
 
 from typing import List
 
-import pandas
+class NumericMLP(torch.nn.Module):
+    def __init__(
+        self,
+        input_dim,
+        output_dim,
+        num_layers,
+        activation,
+    ):
+        super().__init__()
+        if activation == "tanh":
+            self.activation = torch.nn.Tanh()
+        elif activation == "relu":
+            self.activation = torch.nn.ReLU()
+        else:
+            raise ValueError("unkown activation function specified")
+
+        self.lin = torch.nn.ModuleList([])
+        self.lin.append(torch.nn.Linear(input_dim,output_dim))
+        for i in range(1,num_layers):
+            self.lin.append(torch.nn.Linear(output_dim,output_dim))
+
+    def forward(self, numeric_data):
+        output = numeric_data
+        for l in self.lin:
+            output = l(output)
+            output = self.activation(output)
+
+        return output
 
 class NumericFeatureEmbedder(KgeEmbedder):
     def __init__(
@@ -23,49 +50,105 @@ class NumericFeatureEmbedder(KgeEmbedder):
         )
 
         # read config
+        self.regularize = self.check_option("regularize", ["", "lp"])
         self.config.check("train.trace_level", ["batch", "epoch"])
         self.vocab_size = vocab_size
         self.filename = self.get_option("filename")
+        self.num_layers = self.get_option("num_layers")
 
-        # process numeric data
-        df = pandas.read_csv(self.filename, header=None, sep="\t")
-    
+        # load numeric data
+        with open(self.filename,"r") as f:
+            data = list(
+                map(lambda s: s.strip().split("\t"), f.readlines())
+            )
+        
         # returns entities in index order
         entities = self.dataset.entity_ids()
-
+        
         ent_to_idx ={ent: idx for idx, ent in enumerate(entities)}
-        rel_to_idx = {rel: idx for idx, rel in enumerate(df[1].unique())}
+        numeric_data_ent_idx = []
+
+        rel_to_idx = {}
+        numeric_data_rel_idx = []
+        numeric_data = []
+        for t in data:
+            ent = t[0]
+            rel = t[1]
+            value = float(t[2])
+
+            if rel not in rel_to_idx:
+                rel_to_idx[rel] = len(rel_to_idx)
+            
+            numeric_data_ent_idx.append(ent_to_idx[ent])
+            numeric_data_rel_idx.append(rel_to_idx[rel])
+            numeric_data.append(value)
+
+        numeric_data_ent_idx = torch.tensor(
+            numeric_data_ent_idx, dtype=torch.long
+        ) 
+        numeric_data_rel_idx = torch.tensor(
+            numeric_data_rel_idx, dtype=torch.long
+        ) 
+        numeric_data = torch.tensor(
+            numeric_data, dtype=torch.float32
+        )
+
+        # normalize numeric literals
+        if self.get_option("normalization") == "min-max":
+            for rel_idx in rel_to_idx.values():
+                sel = (rel_idx == numeric_data_rel_idx)
+                max_num = torch.max(numeric_data[sel]) 
+                min_num = torch.min(numeric_data[sel]) 
+                numeric_data[sel] = (
+                    (numeric_data[sel] - min_num) / (max_num - min_num + 1e-8)
+                )
+        elif self.get_option("normalization") == "z-score":
+            for rel_idx in rel_to_idx.values():
+                sel = (rel_idx == numeric_data_rel_idx)
+                mean = torch.mean(numeric_data[sel])
+
+                # account for the fact that there might only be a single value
+                # in that case torch.std would result in nan
+                if torch.sum(sel) > 1:
+                    std = torch.std(numeric_data[sel]) 
+                else:
+                    std = 0
+                
+                numeric_data[sel] = (
+                    (numeric_data[sel] - mean) / (std + 1e-8)
+                )
+        else:
+            raise ValueError("Unkown normalization option")
 
         num_lit = torch.zeros(
             [len(ent_to_idx), len(rel_to_idx)], dtype=torch.float32
         )
 
-        for s, p, lit in df.values:
-            try:
-                num_lit[ent_to_idx[s.lower()], rel_to_idx[p]] = float(lit)
-            except KeyError:
-                print(f"Problem for:{s}\t{p}\t{lit}")
-                continue
-        
-        # set dim
-        self.dim = num_lit.shape[1]
-
-        # normalize numeric literals
-        if self.get_option("normalization") == "min-max":
-            max_lit = torch.max(num_lit, dim=0)[0] # get values and ignore indices
-            min_lit = torch.min(num_lit, dim=0)[0] # get values and ignore indices
-            num_lit = (num_lit - min_lit) / (max_lit - min_lit + 1e-8)
-        elif self.get_option("normalization") == "z-score":
-            # do z-scores on each relation separately
-            mean = torch.mean(num_lit, dim=0)
-            std = torch.std(num_lit, dim=0)
-            num_lit = (num_lit - mean) / std
-        else:
-            raise ValueError("Unkown normalization option")
-
+        num_lit[numeric_data_ent_idx,numeric_data_rel_idx] = numeric_data
         # includes all numeric literals for all entities, with the entities
         # being ordered by their index
         self.num_lit = num_lit.to(self.config.get("job.device"))
+
+        if self.num_layers > 0:
+            # initialize numeric MLP
+            self.numeric_mlp = NumericMLP(
+                input_dim=num_lit.shape[1],
+                output_dim=self.dim,
+                num_layers=self.num_layers,
+                activation=self.get_option("activation")
+            )
+
+            if not init_for_load_only:
+                # initialize weights
+                for name, weights in self.numeric_mlp.named_parameters():
+                    # set bias to zero
+                    # https://cs231n.github.io/neural-networks-2/#init 
+                    if "bias" in name:
+                        torch.nn.init.zeros_(weights)
+                    else:
+                        self.initialize(weights)
+        else:
+            self.dim = num_lit.shape[1]
 
         # TODO handling negative dropout because using it with ax searches for now
         dropout = self.get_option("dropout")
@@ -82,7 +165,10 @@ class NumericFeatureEmbedder(KgeEmbedder):
         super().prepare_job(job, **kwargs)
 
     def _embed(self, indexes: Tensor) -> Tensor:
-        return self.num_lit[indexes,:]
+        numeric_data = self.num_lit[indexes,:]
+        if self.num_layers > 0:
+            numeric_data = self.numeric_mlp(numeric_data)
+        return numeric_data
     
     def embed(self, indexes: Tensor) -> Tensor:
         return self._postprocess(self._embed(indexes.long()))
@@ -102,7 +188,39 @@ class NumericFeatureEmbedder(KgeEmbedder):
             )
         )
 
+    def _get_regularize_weight(self) -> Tensor:
+        return self.get_option("regularize_weight")
+
     def penalty(self, **kwargs) -> List[Tensor]:
+        # TODO factor out to a utility method
         result = super().penalty(**kwargs)
-        # there are not parameters to penalize
+        if (
+            self.regularize == "" or
+            self.get_option("regularize_weight") == 0.0 or
+            self.num_layers <= 0
+        ):
+            pass
+        elif self.regularize == "lp":
+            p = (
+                self.get_option("regularize_args.p")
+                if self.has_option("regularize_args.p")
+                else 2
+            )
+            regularize_weight = self._get_regularize_weight()
+            # unweighted Lp regularization
+            parameter_list = []
+            for name, parameter in self.numeric_mlp.named_parameters():
+                if "weight" in name:
+                    parameter_list.append(torch.flatten(parameter))
+            parameters = torch.cat(parameter_list)
+
+            result += [
+                (
+                    f"{self.configuration_key}.L{p}_penalty",
+                    (regularize_weight / p * parameters.norm(p=p) ** p).sum(),
+                )
+            ]
+        else:  # unknown regularization
+            raise ValueError(f"Invalid value regularize={self.regularize}")
+
         return result
